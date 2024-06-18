daf8ab3df6e5563dd2700e0fd893488a95975d8e
Tudor Florea <ftudorel92@yahoo.com>
2024-06-18 09:22:17 +0300

    feat: Added stats.js and a stats setting to turn on and off the stats (#44)

--- src/components/ActionPanel/Settings/index.tsx
+++ src/components/ActionPanel/Settings/index.tsx
@@ -3,18 +3,25 @@ import { useSettingsStore } from "../../../store";
 import ColorPicker from "../../base/ColorPicker";
 import ActionPanelContent from "../common/ActionPanelContent";
 import "./Settings.css";
+import Checkbox from "../../base/Checkbox";
 
 const Settings = () => {
 
-    const { canvasBackground, setCanvasBackground } = useSettingsStore(store => {
+    const { canvasBackground, showStats, setShowStats, setCanvasBackground } = useSettingsStore(store => {
         return {
             canvasBackground: store.canvasBackground,
-            setCanvasBackground: store.setCanvasBackground
+            setCanvasBackground: store.setCanvasBackground,
+            showStats: store.showStats,
+            setShowStats: store.setShowStats
         }
     });
 
     const handleColorChange = (color: ColorResult) => {
         setCanvasBackground(color.hex);
+    };
+
+    const handleShowStatsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setShowStats(e.target.checked);
     };
 
     return (
@@ -23,6 +30,13 @@ const Settings = () => {
                 <span className="setting__text">Canvas color</span>
                 <ColorPicker color={canvasBackground} handleColorChange={handleColorChange} />
             </div>
+            <div className="setting">
+                
+            </div>
+            <div className="setting">
+                    <span className="setting__text">Show stats</span>
+                    <Checkbox checked={showStats} onChange={handleShowStatsChange} />
+                </div>
         </ActionPanelContent>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// import PixiService from './services/PixiService';
 
-// let pixiService = new PixiService();
-
-// pixiService.init();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
